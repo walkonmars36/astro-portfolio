2ed3020fd472168df2786fc7798b4c850bff3439
Mark Lawson <mark@walkonmars.co.uk>
2023-04-26 11:43:26 +0100

    Header, mobile nav and hero section in place

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,18 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".nav-links").classList.toggle("expanded");
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".nav-toggle");
+
+navToggle.addEventListener("click", function () {
+  navToggle.classList.toggle("close");
+
+  const isVisible = nav.getAttribute("data-visible");
+
+  if (isVisible === "true") {
+    nav.setAttribute("data-visible", "false");
+    navToggle.setAttribute("aria-label", "Open Menu");
+    navToggle.setAttribute("aria-expanded", "false");
+  } else {
+    nav.setAttribute("data-visible", "true");
+    navToggle.setAttribute("aria-label", "Close Menu");
+    navToggle.setAttribute("aria-expanded", "true");
+  }
 });
